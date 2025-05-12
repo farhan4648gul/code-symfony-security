955d3bac0dfdf848dbaa32ef6dc7702f86424c15
Farhan Ubuntu 24.04.2 LTS <farhan4648gul@gmail.com>
2025-05-13 00:54:18 +0500

    working with my custom authenticator

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,17 +2,24 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
+    public function __construct(private UserRepository $userRepository, private readonly RouterInterface $router) { 
+    }
+
     public function supports(Request $request): ?bool
     {
 
@@ -32,9 +39,21 @@ class LoginFormAuthenticator extends AbstractAuthenticator
         $password = $request->request->get('password'); 
 
         return new Passport(
-            new UserBadge($email), 
+            new UserBadge($email, function($userIdentifier){
+
+                $user = $this->userRepository->findOneBy(['email' => $userIdentifier]); 
+
+                if (!$user) {
+                    throw new UserNotFoundException( sprintf('%s User not found.', $userIdentifier)); 
+                }
+                
+                return $user; 
+
+            }), 
             new CustomCredentials(function($credentials, $user) {
-                dd($credentials, $user); 
+
+                return $credentials == '12345';     
+                // dd($credentials, $user, 15); 
             }, $password) 
         ); 
 
@@ -44,6 +63,12 @@ class LoginFormAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        return new RedirectResponse(
+            $this->router->generate('app_homepage') 
+        ); 
+
+
+        dd ('here in onAuthenticationSuccess method'); 
         // TODO: Implement onAuthenticationSuccess() method.
     }
 
