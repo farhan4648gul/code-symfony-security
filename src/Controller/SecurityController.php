074ac3bf9a3bf0bc47dc76d1c57d09c82fffe910
Farhan Ubuntu 24.04.2 LTS <farhan4648gul@gmail.com>
2025-05-15 00:43:32 +0500

    working with the Security  from Symfony core

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,12 +7,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Security;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function index(AuthenticationUtils $authenticationUtils, Request $request, Security $security): Response
     {
+
+        dump ( $security ); 
+        dump ( $security->getUser() );  
 
         dump ( $request->getSession() ); 
 
